a7efb0ae35edd1114dec8c34de76edf6a48628e1
zalogarciam <gegarciam95@gmail.com>
2023-04-22 12:40:38 -0500

    Implementing mongoose

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 dotenv.config();
 
 const server = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-server.use(cors({
-    methods: ['GET', 'POST', 'PUT', 'DELETE'] ,
-    origin: '*'
-}))
-server.use(express.json())
+server.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "*",
+  })
+);
+server.use(express.json());
 
 server.listen(PORT, () => {
-    console.log(`Server running ... port: ${PORT}`)
-})
+  console.log(`Server running ... port: ${PORT}`);
+});
+
+function connectDB() {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("Database found");
+    })
+    .catch((e) => {
+      console.error("Error to connecto to the db");
+      console.error(e.message);
+    });
+}
